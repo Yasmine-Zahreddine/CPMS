6d2c76c16943a50bb0ab1b49964f5e161f6dfefe
Yasmine Zd <zahreddineyasmine@gmail.com>
2024-09-02 11:59:42 +0300

    updated the gte configurationi message

--- CPMS_Project/cpms/central_system.py
+++ CPMS_Project/cpms/central_system.py
@@ -195,6 +195,11 @@ class CentralSystem(CP):
         """Handle the StartTransaction request from the charge point."""
         logging.info(
             f"StartTransaction received: id_tag={id_tag}, charge point {self.id} ")
+        configuration = await self.call(call.GetConfiguration())
+        if configuration:
+            logging.info(f"configuration received : {configuration}")
+        else:
+            logging.info("Configuration not received")
         insert_diagnostic(self.supabase, self.id, "StartTransaction", f"StartTransaction received: id_tag={id_tag}")
         logging.info(f"Inserted diagnostic for start transaction.")
         user_id = fetch_user_by_username_or_email(self.supabase, id_tag)
